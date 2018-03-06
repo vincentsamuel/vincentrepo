c2db433d745164b492d39bffa6db0d48a180f084
sss <sss@sss-PC>
2018-03-07 02:10:41 +0530

    com

---
+++ March/src/mar/Vincent.java
@@ -0,0 +1,24 @@
+package mar;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Vincent {
+public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver", "E:\\Edge\\EDGE OLD\\Selenium Folder\\Gecko\\geckodriver.exe");
+	WebDriver d=new FirefoxDriver();
+d.manage().window().maximize();
+d.get("http://newtours.demoaut.com");
+d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+d.findElement(By.name("userName")).sendKeys("Admin");
+d.findElement(By.name("password")).sendKeys("mercury");
+d.findElement(By.name("login")).click();
+
+	
+}
+}
